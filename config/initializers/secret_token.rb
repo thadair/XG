ba744c63a250c987ebd7a20e0777a6cefa11b775
thadair <thaddaniel@gmail.com>
2013-11-17 19:34:08 +0000

    new project xg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Xg::Application.config.secret_key_base = 'df679a82c4507d68cb96ccde5bb7a32912ce52629cc03defb65b17240c91dfd306f35a7bba466ae4883691689f7a324922eed2773e0fe5ad16ee40de6a4fad70'
